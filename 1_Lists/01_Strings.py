88e42d92513b76960c84cbe2d89fde7c67acc585
redfords <joanapiovaroli@gmail.com>
2021-03-02 18:32:10 -0300

    Add Strings WarmUp from CodingBat

--- 1_Lists/01_Strings.py
+++ 1_Lists/01_Strings.py
@@ -54,3 +54,50 @@ def front3(str):
     else:
         return str[:3] + str[:3] + str[:3]
 
+
+""" Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi' """
+
+def string_times(str, n):
+    new_str = ''
+    for i in range(n):
+        new_str = new_str + str
+    return new_str
+
+
+""" Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc' """
+
+def front_times(str, n):
+    front = ''
+    new_str = ''
+    if len(str) < 3:
+        front = str
+    else:
+        front = str[:3]
+    for i in range(n):
+        new_str = new_str + front
+    return new_str
+
+
+""" Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+string_bits('Hello') → 'Hlo'
+string_bits('Hi') → 'H'
+string_bits('Heeololeo') → 'Hello' """
+
+def string_bits(str):
+    new_str = ''
+    for i in range(len(str)):
+        if i % 2 == 0:
+            new_str = new_str + str[i]
+    return new_str
+
+    
