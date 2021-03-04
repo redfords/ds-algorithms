3499613f21633f1421481cb48e2639cad42767b3
redfords <joanapiovaroli@gmail.com>
2021-03-04 03:09:28 -0300

    Add array challenge

--- 1_Intro/01_Strings_CodingBat.py
+++ 1_Intro/01_Strings_CodingBat.py
@@ -42,7 +42,9 @@ def front_back(str):
         return str[len(str) - 1] + str[1:len(str) - 1] + str[0]
 
 
-""" Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+""" Given a string, we'll say that the front is the first 3 chars of the string.
+If the string length is less than 3, the front is whatever is there.
+Return a new string which is 3 copies of the front.
 
 front3('Java') → 'JavJavJav'
 front3('Chocolate') → 'ChoChoCho'
@@ -55,7 +57,8 @@ def front3(str):
         return str[:3] + str[:3] + str[:3]
 
 
-""" Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+""" Given a string and a non-negative int n, return a larger string that is n copies
+of the original string.
 
 
 string_times('Hi', 2) → 'HiHi'
@@ -69,7 +72,9 @@ def string_times(str, n):
     return new_str
 
 
-""" Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+""" Given a string and a non-negative int n, we'll say that the front of the string
+is the first 3 chars, or whatever is there if the string is less than length 3.
+Return n copies of the front;
 
 front_times('Chocolate', 2) → 'ChoCho'
 front_times('Chocolate', 3) → 'ChoChoCho'
@@ -87,7 +92,7 @@ def front_times(str, n):
     return new_str
 
 
-""" Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+""" Given a string, return a new string made of every other char starting with the first.
 
 string_bits('Hello') → 'Hlo'
 string_bits('Hi') → 'H'
@@ -101,3 +106,70 @@ def string_bits(str):
     return new_str
 
     
+""" Given a string, return a string where for every char in the original,
+there are two chars.
+
+double_char('The') → 'TThhee'
+double_char('AAbb') → 'AAAAbbbb'
+double_char('Hi-There') → 'HHii--TThheerree' """
+
+def double_char(str):
+    new_str = ''
+    for letter in str:
+        new_str += letter + letter
+    return new_str
+
+
+""" Return the number of times that the string "hi" appears anywhere in the given string.
+
+count_hi('abc hi ho') → 1
+count_hi('ABChi hi') → 2
+count_hi('hihi') → 2
+ """
+
+def count_hi(str):
+    count = 0
+    for i in range(len(str) - 1):
+        if str[i] == 'h' and str[i + 1] == 'i':
+            count += 1
+    return count
+
+""" Return True if the string "cat" and "dog" appear the same number of times.
+
+cat_dog('catdog') → True
+cat_dog('catcat') → False
+cat_dog('1cat1cadodog') → True """
+
+def cat_dog(str):
+    cat = 0
+    dog = 0
+    for i in range(len(str) - 2):
+        if str[i] == 'c' and str[i + 1] == 'a' and str[i + 2] == 't':
+            cat += 1
+        if str[i] == 'd' and str[i + 1] == 'o' and str[i + 2] == 'g':
+            dog += 1
+    return cat == dog
+
+""" Given two strings, return True if either of the strings appears at
+very end of the other string, ignoring upper/lower case differences.
+
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → Tru """
+
+def end_other(a, b):
+    lower_a = a.lower()
+    lower_b = b.lower()
+
+    if lower_a == lower_b:
+        return True
+
+    if len(a) == len(b) and not lower_a == lower_b:
+        return False
+
+    if len(a) < len(b):
+        lower_b = lower_b[len(b) - len(a):]
+    else:
+        lower_a = lower_a[len(a) - len(b):]
+    
+    return lower_a == lower_b
--- 2_Lists/01_Lists_CodingBat.py
+++ 2_Lists/01_Lists_CodingBat.py
@@ -62,3 +62,15 @@ def string_match(a, b):
     return count
 
     
+""" Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+rotate_left3([1, 2, 3]) → [2, 3, 1]
+rotate_left3([5, 11, 9]) → [11, 9, 5]
+rotate_left3([7, 0, 0]) → [0, 0, 7] """
+
+def rotate_nums(nums):
+    rotated = list()
+    rotated = nums[1:]
+    rotated.append(nums[0])
+    return rotated
+
