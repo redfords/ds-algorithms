f866ae27c5c6f4a14d41e930ffc711dde234184e
redfords <joanapiovaroli@gmail.com>
2021-03-04 14:39:02 -0300

    Update no_of_emails from Coursera

--- 2_Dictionaries/01_Dictionaries_Coursera.py
+++ 2_Dictionaries/01_Dictionaries_Coursera.py
@@ -21,16 +21,13 @@ def most_frequent_word():
     print(bigword, bigcount)
 
 
-"""Exercise: Write a program to read through a mail log, and return how many messages
-have come from each email address.
+"""Write a program to read through a mail log, and return how many messages have
+come from each email address.
 
 Enter file name: mbox-short.txt
-{'gopal.ramasammycook@gmail.com': 1, 'louis@media.berkeley.edu': 3,
+{'gopal.ramasammycook@gmail.com': 1, 'ray@media.berkeley.edu': 3,
 'cwen@iupui.edu': 5, 'antranig@caret.cam.ac.uk': 1,
-'rjlowe@iupui.edu': 2, 'gsilver@umich.edu': 3,
-'david.horwitz@uct.ac.za': 4, 'wagnermr@iupui.edu': 1,
-'zqian@umich.edu': 4, 'stephen.marquard@uct.ac.za': 2,
-'ray@media.berkeley.edu': 1}
+'david.horwitz@uct.ac.za': 4, 'stephen.marquard@uct.ac.za': 2}
 
 Add code to the above program to figure out who has the most messages in the file.
 
