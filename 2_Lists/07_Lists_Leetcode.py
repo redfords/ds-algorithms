d08597f7090496eff34fe869dda1a866613245e2
redfords <joanapiovaroli@gmail.com>
2021-06-06 02:35:12 -0300

    Add smallerNumbersThanCurrent

--- 2_Lists/07_Lists_Leetcode.py
+++ 2_Lists/07_Lists_Leetcode.py
@@ -129,3 +129,27 @@ def numIdenticalPairs(nums):
                 pair += 1
     return pair
 
+"""
+How Many Numbers Are Smaller Than the Current Number
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+That is, for each nums[i] you have to count the number of valid j's such that j != i and
+nums[j] < nums[i].
+Return the answer in an array.
+
+Example 1:
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+"""
+
+def smallerNumbersThanCurrent(nums):
+    smaller = list()
+    for index in range(len(nums)):
+        count = 0
+        for n in nums[: index] + nums[index + 1 :]:
+            if n < nums[index]:
+                count += 1
+        smaller.append(count)
+    return smaller
+
+nums = [8,1,2,2,3]
+print(smallerNumbersThanCurrent(nums))
