cb8a8757da5dfb523e5da14221f7caffd12d4106
redfords <joanapiovaroli@gmail.com>
2021-06-06 01:01:55 -0300

    Add kidsWithCandies

--- 2_Lists/07_Lists_Leetcode.py
+++ 2_Lists/07_Lists_Leetcode.py
@@ -63,5 +63,45 @@ def maximumWealth(accounts):
     sum_accounts = [sum(i) for i in accounts]
     return max(sum_accounts)
 
-accounts = [[1,2,3],[3,2,1]]
-print(maximumWealth(accounts))
+"""
+Kids With the Greatest Number of Candies
+Easy
+
+865
+
+197
+
+Add to List
+
+Share
+Given the array candies and the integer extraCandies, where candies[i] represents the number of
+candies that the ith kid has.
+
+For each kid check if there is a way to distribute extraCandies among the kids such that he or
+she can have the greatest number of candies among them. Notice that multiple kids can have the
+greatest number of candies.
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: 
+Kid 1 has 2 candies and if he or she receives all extra candies (3) will have 5 candies.
+Kid 2 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number.
+Kid 3 has 5 candies and this is already the greatest number of candies among the kids. 
+Kid 4 has 1 candy and even if he or she receives all extra candies will only have 4 candies. 
+Kid 5 has 3 candies and if he or she receives at least 2 extra candies will have the greatest number.
+"""
+
+def kidsWithCandies(candies, extraCandies):
+    max_candies = list()
+    for c in candies:
+        if c + extraCandies >= max(candies):
+            max_candies.append(True)
+        else:
+            max_candies.append(False)
+    return max_candies
+
+# using list comprehension
+def kidsWithCandies(candies, extraCandies):
+    max_candies = max(candies)
+    return [True if (x + extraCandies) >= max_candies else False for x in candies]
+
