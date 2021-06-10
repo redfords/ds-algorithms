b6006561189c3e36825dc32a6a846fcdb113f1cf
redfords <joanapiovaroli@gmail.com>
2021-06-10 00:49:01 -0300

    Add sample dict

--- 2_Dictionaries/03_Dictionaries_IBM.py
+++ 2_Dictionaries/03_Dictionaries_IBM.py
@@ -1,42 +1,3 @@
-# You will need this dictionary for the next two questions:
-
-# soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
-
-# In the dictionary soundtrack_dic what are the keys?
-
-soundtrack_dic = {"The Bodyguard":"1992", "Saturday Night Fever":"1977"}
-soundtrack_dic.keys()
-dict_keys(['The Bodyguard', 'Saturday Night Fever'])
-
-#  In the dictionary soundtrack_dic what are the values?
-
-soundtrack_dic.values()
-dict_values(['1992', '1977'])
-
-# You will need this dictionary for the following questions:
-
-# The Albums Back in Black, The Bodyguard and Thriller have the following music recording sales
-# in millions 50, 50 and 65 respectively.
-
-# Create a dictionary album_sales_dict where the keys are the album name and the sales in millions
-# are the values.
-
-album_sales_dict = {"Back in Black":50, "The Bodyguard":50, "Thriller":65}
-
-# Use the dictionary to find the total sales of Thriller
-
-album_sales_dict["Thriller"]
-
-# Find the names of the albums from the dictionary using the method keys()
-
-album_sales_dict.keys()
-dict_keys(['Back in Black', 'The Bodyguard', 'Thriller'])
-
-# Find the values of the recording sales from the dictionary using the method values
-
-album_sales_dict.values()
-dict_values([50, 50, 65])
-
 """
 Text Analysis 
 You have been recruited by your friend, a linguistics enthusiast, to create a utility tool that can
@@ -44,9 +5,7 @@ perform analysis on a given piece of text.
 ​
 Constructor - takes argument 'text', makes it lower case and removes all punctuation, assume only the
 following punctuation is used: period (.), exclamation mark (!), comma (,) and question mark (?).
-
 freqAll - returns a dictionary of all unique words along with the number of their occurences.
-
 freqOf - returns the frequency of the word passed in argument.
 """
 
@@ -81,13 +40,130 @@ sample = """
 Lorem ipsum dolor! diam amet, consetetur Lorem magna. sed diam nonumy eirmod tempor.
 diam et labore? et diam magna. et diam amet.
 """
+
 t1 = analysedText(sample)
-print(t1.fmtText)
-print(t1.freqAll())
-print(t1.freqOf('lorem'))
+# print(t1.fmtText)
+# print(t1.freqAll())
+# print(t1.freqOf('lorem'))
 
 # Copy file contents to another file
-with open("example1.txt","r") as readFile:
-    with open("example2.txt","w") as writeFile:
-        for line in readFile:
-            writeFile.write(line)
+# with open("example1.txt","r") as readFile:
+#     with open("example2.txt","w") as writeFile:
+#         for line in readFile:
+#             writeFile.write(line)
+
+"""
+Below are the two lists convert it into the dictionary
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+Expected output:
+{'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+"""
+
+def convert(keys, values):
+    nums = dict(zip(keys, values))
+    return nums
+
+"""
+Merge following two Python dictionaries into one
+dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+Expected output:
+{'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+"""
+
+def merge(dict1, dict2):
+    # dict3 = {**dict1, **dict2}
+    dict1.update(dict2)
+    return dict1
+
+"""
+Access the value of key ‘history’ from the below dict
+sample = { 
+   "class":{ 
+      "student":{ 
+         "name":"Mike",
+         "marks":{ 
+            "physics":70,
+            "history":80
+         }
+      }
+   }
+}
+Expected output:
+80
+"""
+def returnMarksHistory(sample):
+    return sample['class']['student']['marks']['history']
+
+"""
+Initialize dictionary with default values
+Given:
+employees = ['Kelly', 'Emma', 'John']
+defaults = {"designation": 'Application Developer', "salary": 8000}
+"""
+
+def initDictionary(employees, defaults):
+    return dict.fromkeys(employees, defaults)
+
+"""
+Create a new dictionary by extracting the following keys from a below dictionary
+Given dictionary:
+sample = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+  
+}
+Keys to extract
+keys = ["name", "salary"]
+Expected output:
+{'name': 'Kelly', 'salary': 8000}
+"""
+def createDict(sample, keys):
+    return {k: sample[k] for k in keys}
+
+"""
+Delete set of keys from a dictionary
+Given:
+sample = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+keysToRemove = ["name", "salary"]
+Expected output:
+{'city': 'New york', 'age': 25}
+"""
+
+def removeKeys(sample, keysToRemove):
+    return  {k: sample[k] for k in sample.keys() - keysToRemove}
+
+"""
+Rename key city to location in the following dictionary
+sample = {
+  "name": "Kelly",
+  "age":25,
+  "salary": 8000,
+  "city": "New york"
+}
+"""
+def rename(sample):
+    sample['location'] = sample.pop('city')
+    return sample
+
+"""
+ Get the key of a minimum value from the following dictionary
+sample = {
+  'Physics': 82,
+  'Math': 65,
+  'history': 75
+}
+Expected output:
+Math
+"""
+
+def getMin(sample):
+    return min(sample, key = sample.get)
