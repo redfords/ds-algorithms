e17e68d929595f4b416ac6992050042899dce479
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-03-21 23:47:49 -0300

    Create 04_Json.py

---
+++ 2_Dictionaries/04_Json.py
@@ -0,0 +1,31 @@
+/* json sample */
+/*
+{
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "hobbies": ["running", "sky diving", "singing"],
+    "age": 35,
+    "children": [
+        {
+            "firstName": "Alice",
+            "age": 6
+        },
+        {
+            "firstName": "Bob",
+            "age": 8
+        }
+    ]
+}
+*/
+
+import json
+
+data = {
+    "president": {
+        "name": "Zaphod Beeblebrox",
+        "species": "Betelgeusian"
+    }
+}
+
+with open("data_file.json", "w") as write_file:
+    json.dump(data, write_file
