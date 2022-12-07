340067d375fa6a72a1b315d9e723d5b1ebf11003
redfords <joanapiovaroli@gmail.com>
2022-12-07 09:40:51 -0300

    Add main and execute

--- TechieDelight/03_Files.py
+++ TechieDelight/03_Files.py
@@ -13,8 +13,50 @@ def _set_spark_session(entidad,fecha):
     return spark
 
 def execute(spark, fecha, fext, entidad, entorno, subent):
-    pass
+    name_cde = {
+            "bsf": "0071",
+            "ber": "0386",
+            "bsc": "0086",
+            "bsj": "0045"
+    }
+    name = name_cde.get(entidad)
+
+    def _sql_from_ref(spark,subent,entity,fecha_extract):
+        pass
+
+    def _format_monto(monto):
+        pass
+
+    def _reverse_format_monto(montoStr):
+        pass
+
+    def _generate_spin_off(final_data,fecha_extract,entity,outdat):
+        pass
+
+    def _generate_header_trailer(final_df,fecha_extract,name):
+        pass
+
+    def _header_and_trailer(final_df,fecha_extract,name,entity,outdat):
+        pass
+
+
 
 if __name__=="__main__":
 
     sys.setdefaultencoding = "utf-8"
+
+    try:
+        params = sys.argv[2].split("-")
+        fecha = params[0].lower()
+        fext = fecha[2:8]
+        entidad = params[1].lower()
+        entorno = sys.argv[1].lower()
+        subent = entorno[0:2]
+
+        spark = _set_spark_session(entidad, fecha)
+
+        execute(spark, fecha, fext, entidad, entorno, subent)
+
+    except Exception as e:
+        print("Error de ejecucion! "+str(e))
+        sys.exit(os.EX_IOERR)
