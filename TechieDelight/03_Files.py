a7f8bd2c80c7776e0aad6974aea4f5f0eea37bfb
redfords <joanapiovaroli@gmail.com>
2022-12-07 09:47:43 -0300

    Add _format_monto

--- TechieDelight/03_Files.py
+++ TechieDelight/03_Files.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-import sys
+import sys, os
 
 def _set_spark_session(entidad,fecha):
     spark = SparkSession\
@@ -21,22 +21,44 @@ def execute(spark, fecha, fext, entidad, entorno, subent):
     }
     name = name_cde.get(entidad)
 
-    def _sql_from_ref(spark,subent,entity,fecha_extract):
-        pass
+    def _sql_from_ref(spark, subent, entity, fecha):
+        final_data_df = spark.sql("""
+            SELECT * FROM {0}_{1}_3ref.Can_Ges_Extract_Prima_Individuos_FT_Pre_Archivo
+            WHERE t_cde in ("31", "2P", "28", "70", "61", "62", "18", "30", "40", "68",
+            "34", "35", "37", "R2", "2H", "3H", "38","70", "OP", "F1", "F2")      
+            AND fecha_proceso = {2}
+        """.format(subent, entity, fecha)
+        )
+
+        return final_data_df
 
     def _format_monto(monto):
+        last_char = monto[-1]
+        valor = {
+                '0': '{',
+                '1': 'A',
+                '2': 'B',
+                '3': 'C',
+                '4': 'D',
+                '5': 'E',
+                '6': 'F',
+                '7': 'G',
+                '8': 'H',
+                '9': 'I'
+        }[last_char]
+        output = monto[:-1] + valor
+        return output
+
+    def _reverse_format_monto(monto_str):
         pass
 
-    def _reverse_format_monto(montoStr):
+    def _generate_spin_off(final_data, fecha_extract, entity, outdat):
         pass
 
-    def _generate_spin_off(final_data,fecha_extract,entity,outdat):
+    def _generate_header_trailer(final_df, fecha_extract, name):
         pass
 
-    def _generate_header_trailer(final_df,fecha_extract,name):
-        pass
-
-    def _header_and_trailer(final_df,fecha_extract,name,entity,outdat):
+    def _header_and_trailer(final_df, fecha_extract, name, entity, outdat):
         pass
 
 
@@ -58,5 +80,5 @@ if __name__=="__main__":
         execute(spark, fecha, fext, entidad, entorno, subent)
 
     except Exception as e:
-        print("Error de ejecucion! "+str(e))
+        print("Error de ejecucion: "+str(e))
         sys.exit(os.EX_IOERR)
