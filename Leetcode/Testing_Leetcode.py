34c369bae24d28027857ae60f2e64da2f24800b0
redfords <joanapiovaroli@gmail.com>
2023-01-15 01:15:20 -0300

    Add string functions tests

---
+++ Leetcode/Testing_Leetcode.py
@@ -0,0 +1,32 @@
+import a_Strings_Leetcode as a
+import sys
+import traceback
+
+def a_strings():
+    str = "255.100.50.0"
+    a.defang_ip_addr(str)
+
+    operations = ["X++","++X","--X","X--"]
+    a.final_value_after_operations(operations)
+
+    jewels = "aA"
+    stones = "aAAbbbb"
+    a.num_jewels_in_stones(jewels, stones)
+
+    sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+    a.most_words_found(sentences)
+
+    command = "G()(al)"
+    a.interpret(command)
+
+    s = "codeleet"
+    indices = [4,5,6,7,0,2,1,3]
+    print(a.restore_string(s, indices))
+
+if __name__=="__main__":
+    try:
+        a_strings()
+
+    except Exception as e:
+        print(traceback.format_exc())
+        sys.exit()
