a97ef4dfa1f79beab4c77f25209c62617036795c
redfords <joanapiovaroli@gmail.com>
2023-01-17 01:22:39 -0300

    Add unique_morse_representations

--- Leetcode/a_Strings_Leetcode.py
+++ Leetcode/a_Strings_Leetcode.py
@@ -282,3 +282,52 @@ Output: true
 
 def array_strings_are_equal(word1, word2):
     return ''.join(word1) == ''.join(word2)
+
+"""
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...".
+We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+
+Example 1:
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+"""
+
+def unique_morse_representations(words):
+    morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    unique_words = {
+        "".join(morse[ord(c) - 97] for c in word)
+        for word in words
+    }
+    return len(unique_words)
+
+"""
+Given a VPS represented as string s, return the nesting depth of s.
+
+Example 1:
+
+Input: s = "(1+(2*3)+((8)/4))+1"
+Output: 3
+
+Example 2:
+Input: s = "(1)+((2))+(((3)))"
+Output: 3
+"""
+
+def max_depth(s):
+    depth = 0
+    open = 0
+    for c in s:
+        if c == '(':
+            open += 1
+            depth = max(depth, open)
+        if c == ')':
+            open -= 1
+    return depth
