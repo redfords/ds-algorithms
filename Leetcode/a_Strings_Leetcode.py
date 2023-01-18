f9ac424cf0e9ceb231be79a295e7ce9d652ddc1a
redfords <joanapiovaroli@gmail.com>
2023-01-18 01:56:06 -0300

    Add truncate_sentence

--- Leetcode/a_Strings_Leetcode.py
+++ Leetcode/a_Strings_Leetcode.py
@@ -331,3 +331,33 @@ def max_depth(s):
         if c == ')':
             open -= 1
     return depth
+
+"""
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s and an integer k. You want to truncate s such that it contains only the first k words. Return s after truncating it.
+
+Example 1:
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+"""
+
+def truncate_sentence(s, k):
+    s = s.split()
+    return ' '.join(s[:k])
+
+"""
+You are given a string s, where every two consecutive vertical bars '|' are grouped into a pair.
+In other words, the 1st and 2nd '|' make a pair, the 3rd and 4th '|' make a pair, and so forth.
+
+Return the number of '*' in s, excluding the '*' between each pair of '|'.
+
+Note that each '|' will belong to exactly one pair.
+
+Example 1:
+Input: s = "l|*e*et|c**o|*de|"
+Output: 2
+"""
+
