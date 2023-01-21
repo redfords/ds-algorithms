8516fa1b501d0cf4d8ac69da87136eb43f2c6e65
redfords <joanapiovaroli@gmail.com>
2023-01-21 13:06:31 -0300

    Add replace_digits

--- Leetcode/a_Strings_Leetcode.py
+++ Leetcode/a_Strings_Leetcode.py
@@ -495,3 +495,24 @@ Output: 3
 
 def num_of_strings(patterns, word):
     return sum(1 for p in patterns if p in word)
+
+"""
+You are given a 0-indexed string s that has lowercase English letters in its even indices and digits in its odd indices.
+
+There is a function shift(c, x), where c is a character and x is a digit, that returns the xth character after c.
+
+For example, shift('a', 5) = 'f' and shift('x', 0) = 'x'.
+For every odd index i, you want to replace the digit s[i] with shift(s[i-1], s[i]).
+
+Return s after replacing all digits. It is guaranteed that shift(s[i-1], s[i]) will never exceed 'z'.
+
+Example 1:
+Input: s = "a1c1e1"
+Output: "abcdef"
+"""
+
+def replace_digits(s):
+    res = list(s)
+    for i in range(1, len(res), 2):
+        res[i] = chr(ord(res[i-1]) + int(res[i]))
+    return "".join(res)
