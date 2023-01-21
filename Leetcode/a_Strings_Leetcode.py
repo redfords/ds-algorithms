d2f310fa5cd070b2878da22345e5aeee385b5fab
redfords <joanapiovaroli@gmail.com>
2023-01-21 03:59:43 -0300

    Add num_of_strings

--- Leetcode/a_Strings_Leetcode.py
+++ Leetcode/a_Strings_Leetcode.py
@@ -482,5 +482,16 @@ def remove_outer_parentheses(s):
         elif s[i] == ')' and count == 1:
             count -= 1
     return res
-        
 
+"""
+Given an array of strings patterns and a string word, return the number of strings in patterns that exist as a substring in word.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+Input: patterns = ["a","abc","bc","d"], word = "abc"
+Output: 3
+"""
+
+def num_of_strings(patterns, word):
+    return sum(1 for p in patterns if p in word)
