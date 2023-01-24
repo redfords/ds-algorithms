e98c1aa749f2ed7d1ec2e52622e5c59a32dd6eca
redfords <joanapiovaroli@gmail.com>
2023-01-23 23:41:11 -0300

    Add dest_city

--- Leetcode/a_Strings_Leetcode.py
+++ Leetcode/a_Strings_Leetcode.py
@@ -546,3 +546,15 @@ Output: "pppz"
 
 def generate_the_string(n):
     return 'a' * (n - 1) + 'b' if n % 2 == 0 else 'a' * n
+
+"""
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi.
+Return the destination city, that is, the city without any path outgoing to another city.
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+Input: paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+Output: "Sao Paulo" 
+"""
+
+def dest_city(paths):
+    pass
