5ee6b27db061a2b11ddd3c40afac7938a0cde2ea
redfords <joanapiovaroli@gmail.com>
2023-01-24 22:08:37 -0300

    build_array

---
+++ Leetcode/b_Arrays_Leetcode.py
@@ -0,0 +1,12 @@
+"""
+Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
+for each 0 <= i < nums.length and return it.
+A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+Input: nums = [0,2,1,5,3,4]
+Output: [0,1,2,4,5,3]
+"""
+
+def build_array(nums):
+    pass
+
