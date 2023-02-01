5425de8ee7c95628df775f8960161feac9ad8b68
redfords <joanapiovaroli@gmail.com>
2023-02-01 09:58:21 -0300

    Update download link

--- TechieDelight/d_Json_TechieDelight.py
+++ TechieDelight/d_Json_TechieDelight.py
@@ -8,7 +8,7 @@ def json_actions():
     todos = json.loads(response.text)
     todos_format = json.dumps(todos, indent=4)
     print(todos_format)
-    
+
     # json.loads takes in a string and returns a json object
     # json.dumps takes in a json object and returns a string
 
@@ -54,8 +54,32 @@ def json_actions():
     max_users = " and ".join(users)
     print(max_users)
 
+    """ 
+    JSON Parsing
+    Source: https://github.com/chrhobbs/exercise-python-json-parsing
+
+    Exercise 1
+    Using data file 'interface-data.json', create output that resembles the following:
+
+    Interface Status
+    ================================================================================
+    DN                                                 Description           Speed    MTU  
+    -------------------------------------------------- --------------------  ------  ------
+    topology/pod-1/node-201/sys/phys-[eth1/33]                              inherit   9150 
+    topology/pod-1/node-201/sys/phys-[eth1/34]                              inherit   9150 
+    topology/pod-1/node-201/sys/phys-[eth1/35]                              inherit   9150 
 
 
+    Exercise 2
+    Use the data to create a similar table which displays the average / max number of
+    passengers per month per airline traveling through BWI airport. Format number numbers
+    to 2 decimal places as needed.
+
+    Read from https://opendata.maryland.gov/resource/6jva-hr4v.json
+    """
+
+def read_json():
+    pass
 
 if __name__=="__main__":
     try:
@@ -64,29 +88,3 @@ if __name__=="__main__":
     except Exception as e:
         print(traceback.format_exc())
         sys.exit()
-
-
-""" 
-JSON Parsing
-Source: https://github.com/chrhobbs/exercise-python-json-parsing
-
-Exercise 1
-Using data file 'interface-data.json', create output that resembles the following:
-
-Interface Status
-================================================================================
-DN                                                 Description           Speed    MTU  
--------------------------------------------------- --------------------  ------  ------
-topology/pod-1/node-201/sys/phys-[eth1/33]                              inherit   9150 
-topology/pod-1/node-201/sys/phys-[eth1/34]                              inherit   9150 
-topology/pod-1/node-201/sys/phys-[eth1/35]                              inherit   9150 
-
-
-Exercise 2
-Use the data to create a similar table which displays the average / max number of
-passengers per month per airline traveling through BWI airport. Format number numbers
-to 2 decimal places as needed.
-
-Read from http://data.maryland.gov/api/views/6jva-hr4v/rows.json?accessType=DOWNLOAD
-"""
-
