e91117116a73bb653e98ede4107511fc49eec62c
redfords <joanapiovaroli@gmail.com>
2023-02-03 23:42:06 -0300

    Add json example from Real Python

---
+++ TechieDelight/e_Json_RealPython.py
@@ -0,0 +1,45 @@
+import json
+import traceback
+import sys
+
+def test():
+    data = {
+    "president": {
+        "name": "Zaphod Beeblebrox",
+        "species": "Betelgeusian"
+    }
+}
+
+    with open("data_file.json", "w") as write_file:
+        json.dump(data, write_file)
+
+    json_string = json.dumps(data, indent=4)
+    print(json_string)
+
+    with open("data_file.json", "r") as read_file:
+        data = json.load(read_file)
+
+    json_string = """
+    {
+        "researcher": {
+            "name": "Ford Prefect",
+            "species": "Betelgeusian",
+            "relatives": [
+                {
+                    "name": "Zaphod Beeblebrox",
+                    "species": "Betelgeusian"
+                }
+            ]
+        }
+    }
+    """
+    data = json.loads(json_string)
+    print(data)
+
+if __name__=="__main__":
+    try:
+        test()
+
+    except Exception as e:
+        print(traceback.format_exc())
+        sys.exit()
