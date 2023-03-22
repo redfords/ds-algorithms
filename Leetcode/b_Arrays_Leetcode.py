b95df22702ea451c5fb6d20161ec17e3a7dc0821
redfords <joanapiovaroli@gmail.com>
2023-03-21 23:46:33 -0300

    Add leftRigthDifference

--- Leetcode/b_Arrays_Leetcode.py
+++ Leetcode/b_Arrays_Leetcode.py
@@ -37,3 +37,20 @@ def difference_of_sum(nums):
     s = sum(nums)
     d = sum(int(d) for num in nums for d in str(num))
     return abs(s - d)
+
+"""
+Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+
+answer.length == nums.length.
+answer[i] = |leftSum[i] - rightSum[i]|.
+
+Where:
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+
+Return the array answer.
+
+Input: nums = [10,4,8,3]
+Output: [15,1,11,22]
+"""
+
