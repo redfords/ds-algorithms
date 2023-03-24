6a9ff6b3a71963e0aaec8d7ec1b78632e1d5caa4
redfords <joanapiovaroli@gmail.com>
2023-03-23 22:15:00 -0300

    Add largest_local

--- Leetcode/Testing_Leetcode.py
+++ Leetcode/Testing_Leetcode.py
@@ -119,6 +119,8 @@ def b_arrays():
     k = 1
     b.count_k_difference(nums, k)
 
+    grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+    print(b.largest_local(grid))
 
 if __name__=="__main__":
     try:
--- Leetcode/b_Arrays_Leetcode.py
+++ Leetcode/b_Arrays_Leetcode.py
@@ -81,4 +81,18 @@ def count_k_difference(nums, k):
             if abs(n - x) == k:
                 count += 1
     return count
+
+"""
+You are given an n x n integer matrix grid.
+
+Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
+
+maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
+In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+
+Input: grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+Output: [[9,9],[8,6]]
+"""
+
+def largest_local(grid):
     
