cfd8229b3eef2425e104e931fce0624bea20e39b
redfords <joanapiovaroli@gmail.com>
2023-03-23 22:49:27 -0300

    Add largest_local

--- Leetcode/b_Arrays_Leetcode.py
+++ Leetcode/b_Arrays_Leetcode.py
@@ -90,9 +90,18 @@ Generate an integer matrix maxLocal of size (n - 2) x (n - 2) such that:
 maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1.
 In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
 
-Input: grid = [[9,9,8,1],[5,6,2,6],[8,2,6,4],[6,2,2,2]]
+Input: grid = [
+    [9,9,8,1],
+    [5,6,2,6],
+    [8,2,6,4],
+    [6,2,2,2]]
 Output: [[9,9],[8,6]]
 """
 
 def largest_local(grid):
+    for i in range(len(grid) - 2):
+        print(i)
+        for j in range(len(grid[0]) - 3):
+            print(j)
+            print(grid[i][j])
     
