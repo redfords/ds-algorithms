e2c16321fe7e94e80ce4d292cc2fd0dd13269fbd
redfords <joanapiovaroli@gmail.com>
2023-03-29 23:54:06 -0300

    Add script

---
+++ TechieDelight/get_last_date.py
@@ -0,0 +1,42 @@
+#Script de PySpark para obtener la fecha de proceso maxima de una tabla
+
+import sys,traceback,os,subprocess
+from pyspark.sql import SparkSession
+
+def _setSparkSession(entidad,fechaextract):
+    spark = SparkSession\
+        .builder\
+        .appName("Omni Find Last Process Dates ({0} - {1})".format(entidad,fechaextract))\
+        .enableHiveSupport()\
+        .master("local[2]")\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("WARN")
+
+    return spark
+
+def _get_table_max_process_date(table_location):
+        
+        cmd = "hdfs dfs -ls %s" % (table_location)
+        files = subprocess.check_output(cmd, shell=True).strip().split('\n')
+        for path in files:
+            print(path)
+        #path_table = 'hdfs://' + outdat + '/spin_off/spin_off_reborn_{}_{}'.format(entity, fecha_extract)
+        #parseo.coalesce(1).write.option("sep", "|").format("com.databricks.spark.csv").option("header", "false").option(
+        #            "ignoreLeadingWhiteSpace", "false").option("ignoreTrailingWhiteSpace", "false").mode("append").save(path_spin_off)
+
+        #print("El spinoff de fecha: {} y entidad: {} se genero correctamente".format(fecha_extract, entity))
+        return ""
+
+#Execution
+if __name__=="__main__":
+   try:
+       #Recibe por parametro la location de la tabla y su alias
+       table_loc=sys.argv[1]
+       table_alias=sys.argv[2]
+       print("Tabla: "+table_alias+", Max fecha_proceso: "+_get_table_max_process_date(table_loc))
+       
+   except Exception as e:
+       print("There was a problem getting the max process date!! "+str(e))
+       print(traceback.print_exc())
+       traceback.print_exc()
+       sys.exit(os.EX_IOERR)
