01e197b4a6e0b01df2b3823b864db02fdd8ecc31
redfords <joanapiovaroli@gmail.com>
2023-05-20 03:14:55 -0300

    Add result formatting

--- TechieDelight/check_file.py
+++ TechieDelight/check_file.py
@@ -5,29 +5,37 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-def _check_file(path,header,delimiter):
-
+def _check_file(path, header, delimiter):
     errors = list()
 
     with open(path, "r") as file:
         lines = file.readlines()
 
-        num_lines = len(lines)
-
-        num_cols = len(lines[0].split(delimiter))
+        # Get number of columns from first line
+        try:
+            num_cols = len(lines[0].split(delimiter))
+            if num_cols == 1:
+                errors.append("First line with no delimiters!")
+        except:
+            print("File is empty")
 
         line_count = 0
+
         for line in lines:
             line_count += 1
+            
             if line.strip() == "":
                 errors.append("Line break! At line " + str(line_count))
+            
             elif len(line.split(delimiter)) != num_cols:
-                errors.append("No same amount of records! At line " + str(line_count))
-    
-        print(header, delimiter)
-        print(num_lines, num_cols)
+                errors.append("No same amount of columns! At line " + str(line_count))
+
+        if header == "h":
+            line_count -= 1
 
     file.close()
+
+    return [errors, line_count]
 
 if __name__ == "__main__":
     try:
@@ -35,16 +43,21 @@ if __name__ == "__main__":
         header = sys.argv[2]
         delimiter = sys.argv[3]
 
-        result = _check_file(path,header,delimiter)
-        print(result)
+        result = _check_file(path, header, delimiter)
 
-        # if (result[0]==3):
-        #     print(bcolors.OK_GREEN + "SUCCESS! The file is OK!" + bcolors.ENDC)
-        # else:
-        #     print(bcolors.FAIL + "WARNING! The file is NOT OK!" + bcolors.ENDC)
-        #     print("")
-        #     print("Errors detected: ")
-        #     print(result[1])
+        if not result[0]:
+            print(bcolors.OK_GREEN + "SUCCESS! The file is OK!" + bcolors.ENDC)
+        else:
+            print(bcolors.FAIL + "WARNING! The file is NOT OK!" + bcolors.ENDC)
+            print("")
+            print("Errors detected: ")
+            for r in result[0]:
+                print(r)
+        
+        print("")
+        print("COUNTS:")
+        print("The file has {0} records.".format(result[1])) 
+        print("")
 
     except Exception as e:
         print(e)
