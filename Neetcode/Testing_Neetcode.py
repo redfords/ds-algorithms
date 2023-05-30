d3180794743157ffbb12ebfc5f2f07c293188f8f
redfords <joanapiovaroli@gmail.com>
2023-05-30 17:37:53 -0300

    Change is_anagram to dictionary instead of string

--- Neetcode/Testing_Neetcode.py
+++ Neetcode/Testing_Neetcode.py
@@ -5,13 +5,19 @@ import traceback
 def a_strings():
     nums = [1,2,3,4]
     # nums = [1,1,1,3,3,4,3,2,4,2,5]
-    result = a.contains_duplicate(nums)
+    a.contains_duplicate(nums)
 
-    print(result)
+    # s = "anagram"
+    # t = "nagaram"
+    s = "rat"
+    t = "car"
+
+    return a.is_anagram(s, t)
 
 if __name__=="__main__":
     try:
-        a_strings()
+        result = a_strings()
+        print(result)
 
     except Exception as e:
         print(traceback.format_exc())
--- Neetcode/a_Arrays_Hashing.py
+++ Neetcode/a_Arrays_Hashing.py
@@ -10,3 +10,33 @@ def contains_duplicate(nums):
             return True
         distinct.add(num)
     return False
+
+    # distinct = set(nums)
+    # return len(nums) != len(distinct)
+
+"""
+242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+
+def is_anagram(s, t):
+    if len(s) != len(t):
+        return False
+    
+    count_s = dict()
+    count_t = dict()
+
+    for i in range(len(s)):
+        count_s[s[i]] = 1 + count_s.get(s[i], 0)
+        count_t[t[i]] = 1 + count_t.get(t[i], 0)
+
+    return count_s == count_t
+
+
+    # for c in s:
+    #     if c in t:
+    #         t = t.replace(c, '', 1)
+    #     else:
+    #         return False
+    # return len(t) == 0
