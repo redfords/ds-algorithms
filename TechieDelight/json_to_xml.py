1750c22bda03ae8b94150a0712a1db9f7973eb20
redfords <joanapiovaroli@gmail.com>
2023-06-30 23:10:01 -0300

    Add xml template

--- TechieDelight/json_to_xml.py
+++ TechieDelight/json_to_xml.py
@@ -24,10 +24,71 @@ def generate_xml(data: json, group: str, pan_no: str) -> list:
 
 def fill_xml(trx_obj: str, group: str) -> str:
     """Returns the xml template with the trx data"""
-    xml_template = """
+    return """<?xml version="1.0" encoding="UTF-8"?>
+                    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.ws.spfservices.redlink.com.ar/">
+                    <soapenv:Header>
+                        <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" soapenv:mustUnderstand="1">
+                            <wsse:UsernameToken wsu:Id="UsernameToken-C734DACD44C7F029F816195003929764">
+                                <wsse:Username>%s</wsse:Username>
+                                <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">%s</wsse:Password>
+                            </wsse:UsernameToken>
+                        </wsse:Security>
+                    </soapenv:Header>
+                    <soapenv:Body>
+                        <ser:uploadTransaction>
+                            <!--Optional:-->
+                            <requerimientoUploadTransaction>
+                                <cabecera>
+                                <idRequerimiento>%s</idRequerimiento>
+                                <ipCliente>%s</ipCliente>
+                                <timeStamp>%s</timeStamp>
+                                <idEntidad>%s</idEntidad>
+                                <canal>%s</canal>
+                                </cabecera>
+                                <datosTransaction>
+                                <fiidTerm>%s</fiidTerm>
+                                <termId>%s</termId>
+                                <fiidCard>%s</fiidCard>
+                                <pan>%s</pan>
+                                <seqNum>%s</seqNum>
+                                <tranDat>%s</tranDat>
+                                <tranTim>%s</tranTim>
+                                <typ>%s</typ>
+                                <typCde>%s</typCde>
+                                <postDat>%s</postDat>
+                                <tranCde>%s</tranCde>
+                                <fromAcct>%s</fromAcct>
+                                <tipoDep>%s</tipoDep>
+                                <toAcct>%s</toAcct>
+                                <importe>%s</importe>
+                                <respCde>%s</respCde>
+                                <issuerFiid>%s</issuerFiid>
+                                <termType>%s</termType>
+                                <tipoCambio>%s</tipoCambio>
+                                <tipoCambioC>%s</tipoCambioC>
+                                <cuota>%s</cuota>
+                                <ente>%s</ente>
+                                <termLn>%s</termLn>
+                                <crncyCde>%s</crncyCde>
+                                <cardType>%s</cardType>
+                                <codigoPais>%s</codigoPais>
+                                <locTerm>%s</locTerm>
+                                <denEstabl>%s</denEstabl>
+                                <establecimiento>%s</establecimiento>
+                                <rubro>%s</rubro>
+                                <cvvrc>%s</cvvrc>
+                                <direccionIp>%s</direccionIp>
+                                <canal>%s</canal>
+                                <producto>%s</producto>
+                                <tel>%s</tel>
+                                <crdLn>%s</crdLn>
+                                <codigoPaisEntidad>%s</codigoPaisEntidad>
+                                </datosTransaction>
+                            </requerimientoUploadTransaction>
+                        </ser:uploadTransaction>
+                    </soapenv:Body>
+                    </soapenv:Envelope>
     """
-
-    return xml_template
 
 if __name__ == "__main__":
     sys.setdefaultencoding = "utf-8"
