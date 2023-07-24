880b29e7815c1a5bf5fa82922b38fc5e3c84b746
redfords <joanapiovaroli@gmail.com>
2023-07-24 02:27:54 -0300

    Add date and time udf

--- TechieDelight/reporte_extendido.py
+++ TechieDelight/reporte_extendido.py
@@ -1,7 +1,7 @@
 from sys import argv
 from traceback import print_exc
 from pyspark.sql.types import StringType
-from pyspark.sql.functions import udf, col, when, concat
+from pyspark.sql.functions import udf, col, when, lit
 from pyspark.sql.session import SparkSession
 
 
@@ -27,6 +27,25 @@ class OmniReporteExtendido():
             output = monto[:-1] + valor
             return output
         udf_monto = udf(format_monto, StringType())
+
+        def format_fecha(tran_dat):
+            output = [
+                '20' + str(tran_dat[0:2]),
+                str(tran_dat[2:4]),
+                str(tran_dat[4:6])
+            ]
+            return '-'.join(output)
+        udf_fecha = udf(format_fecha, StringType())
+
+        def format_hora(tran_tim):
+            output = [
+                str(tran_tim[0:2]),
+                str(tran_tim[2:4]),
+                str(tran_tim[4:6]),
+                '.' + str(tran_tim[6:8])
+            ]
+            return ':'.join(output)
+        udf_hora = udf(format_hora, StringType())
 
         # app_name = "Reporte extendido "
         # spark = SparkSession.builder.appName(app_name).master('yarn').enableHiveSupport()\
@@ -54,48 +73,46 @@ class OmniReporteExtendido():
         self.contexts = {}
         self.contexts['spark'] = spark
         data = self.contexts['spark'].read.csv('rep_mens.csv', sep='|', header=True, inferSchema = False)
+
+        # filter by t_cde and t_from
         data = data.filter("t_cde = '18' AND t_from = '01'")
 
+        # format monto
         data = data.withColumn("monto", udf_monto("amt_1"))\
             .withColumn("monto", col("monto")/100)
-        data = data.withColumn("amt", col("monto").cast('decimal(16,2)'))
+        data = data.withColumn("importe", col("monto").cast('decimal(16,2)'))
 
-        (when(col("orig_crncy_cde") == '032', 'ARS')
-            .otherwise('USD'))
+        # update currency code and cuil
+        data = data.withColumn("moneda", when(col("orig_crncy_cde") == "032", "ARS").otherwise("USD"))
+        data = data.withColumn("cuil", col("crd_pan").substr(6, 11))
+
+        data = data.select("tran_dat", "tran_tim", "term_typ", "resp_byte_2", "importe", "moneda", "cuil")
+
+        # format date and time
+        data = data.withColumn("fecha", udf_fecha("tran_dat"))\
+            .withColumn("hora", udf_hora("tran_tim"))
+
+        # add constant columns
+        data = data.withColumn("codigo", lit("180100"))\
+            .withColumn("tipo", lit("INVERSIÓN EN PLAZO FIJO"))\
+            .withColumn("cantidad", lit("1"))
         
-        print(data.columns)
-        data.printSchema()
+        # rename columns
+        data = data.withColumnRenamed("term_typ", "canal")\
+            .withColumnRenamed("resp_byte_2", "codigo_respuesta")
+        
+        data = data.orderBy("tran_dat")
+
+        data = data.select("canal", "moneda", "codigo", "tipo", "cuil", "fecha", "hora", "importe", "cantidad")
         data.show(5)
 
 
         # plazo fijo cc pesos
         # data = self.contexts['spark'].read.csv('hdfs://' + path_input, sep='|', header=True, inferSchema = False)
-        # where  T_cde = "18" and T_from = "01" 
-        
 
-        # convierto amt_1 en monto
-        # data = data.withColumn("monto", udf_monto("amt_1"))\
-        #     .withColumn("monto", col("monto")/100)
-        
-        # reemplazo decimal
-        # amt = data.withColumn("amt", col("monto").cast('decimal(16,2)'))
 
-        # convierto orig_crncy_cde en moneda
-
-        # cuil, fecha, hora
-
-        # codigo (new, 180100), tipo (new "desc"), cantidad (new, 1)
-        # term_typ (rename as canal), resp_byte_2 (rename as codigo_respuesta)
-        # order by tran_dat
-        
-
-        
-        # amt.show()
-        # amt=amt.orderBy("Codigo")        
         # amt.coalesce(1).write.option("sep", "|").format("com.databricks.spark.csv").option("header","true")\
         #     .option("ignoreLeadingWhiteSpace", "false").option("ignoreTrailingWhiteSpace", "false").mode("append").save(path_output)
-
-
 
 if __name__=="__main__":
     try:
