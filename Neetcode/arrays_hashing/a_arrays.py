9fd81d62a82d459be9f90f03b4b49c6e0743c624
redfords <joanapiovaroli@gmail.com>
2024-01-01 23:41:24 -0300

    Create a_arrays.py

---
+++ Neetcode/arrays_hashing/a_arrays.py
@@ -0,0 +1,75 @@
+"""
+217. Contains Duplicate
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
+
+def contains_duplicate(nums):
+    pass
+
+"""
+242. Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+
+def is_anagram(s, t):
+    pass
+
+"""
+1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+"""
+
+def two_sum(nums, target):
+    pass
+
+"""
+9. Palindrome Number
+Given an integer x, return true if x is a palindrome, and false otherwise.
+"""
+
+def is_palindrome(x):
+    pass
+    
+
+"""
+14. Longest Common Prefix
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+"""
+
+def longest_common_prefix(strs):
+    pass
+
+"""
+20. Valid Parentheses
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+
+def is_valid(s):
+    pass
+"""
+26. Remove Duplicates from Sorted Array
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only
+once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+"""
+
+def remove_duplicates(nums):
+        
+    pass
+
+"""
+28. Find the Index of the First Occurrence in a String
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+"""
+
+def str_str(haystack, needle):
+    pass
+
+    
